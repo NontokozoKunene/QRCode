ad4e975215dc63456411c9787b3c4f9bd1058555
unknown <Nontokozo92kunene@gmail.com>
2020-10-27 10:04:39 +0200

    updated

---
+++ GenerateQRcode/GenerateQRcode/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QRCoder;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace GenerateQRcode.Controllers
+{
+   public class HomeController : Controller
+{
+    // GET: Home
+    public ActionResult Index()
+    {
+        return View();
+    }
+ 
+    [HttpPost]
+    public ActionResult Index(string qrcode)
+    {
+
+            qrcode += " Time out for you\n you well end";
+        using (MemoryStream ms = new MemoryStream())
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qRCodeData = qrGenerator.CreateQrCode(" ", QRCodeGenerator.ECCLevel.Q);
+                QRCode qRQcode = new QRCode(qRCodeData);
+
+                 
+            using (Bitmap bitMap = qRQcode.GetGraphic(20))
+            {
+                bitMap.Save(ms, ImageFormat.Png);
+                ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+            }
+        }
+ 
+        return View();
+    }
+       
+      
+
+        
+    }
+}
---
+++ GenerateQRcode/GenerateQRcode/Controllers/QrimagesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace GenerateQRcode.Controllers
+{
+    public class QrimagesController : Controller
+    {
+        private QRDBEntities db = new QRDBEntities();
+
+        // GET: Qrimages
+        public ActionResult Index()
+        {
+            return View(db.Qrimages.ToList());
+        }
+
+        // GET: Qrimages/Details/5
+        public ActionResult Details(byte[] id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Qrimage qrimage = db.Qrimages.Find(id);
+            if (qrimage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(qrimage);
+        }
+
+        // GET: Qrimages/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Qrimages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "qr_code")] Qrimage qrimage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Qrimages.Add(qrimage);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(qrimage);
+        }
+
+        // GET: Qrimages/Edit/5
+        public ActionResult Edit(byte[] id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Qrimage qrimage = db.Qrimages.Find(id);
+            if (qrimage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(qrimage);
+        }
+
+        // POST: Qrimages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "qr_code")] Qrimage qrimage)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(qrimage).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(qrimage);
+        }
+
+        // GET: Qrimages/Delete/5
+        public ActionResult Delete(byte[] id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Qrimage qrimage = db.Qrimages.Find(id);
+            if (qrimage == null)
+            {
+                return HttpNotFound();
+            }
+            return View(qrimage);
+        }
+
+        // POST: Qrimages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(byte[] id)
+        {
+            Qrimage qrimage = db.Qrimages.Find(id);
+            db.Qrimages.Remove(qrimage);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
